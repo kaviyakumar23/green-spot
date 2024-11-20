ea1c89289c1fd0f1a560393f140645d050e1f8b8
kaviya <kaviyakumar23@gmail.com>
2024-11-20 20:33:51 +0530

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
-import vercel from "vite-plugin-vercel";
-
+import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vercel()],
+  plugins: [react()],
 });
